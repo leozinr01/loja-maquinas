178108a7539fb4a40015ac2789327bcec0b65143
Leonardo Freitas <166871181+leozinr01@users.noreply.github.com>
2025-08-30 14:37:57 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,14 +1,18 @@
+# Importa a função para criar conexão com o banco
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+# Define a URL do banco de dados a partir de uma variável de ambiente
 DB_URL = os.getenv("DATABASE_URL", "sqlite:///./loja.db")
 connect_args = {"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
 engine = create_engine(DB_URL, echo=False, future=True, connect_args=connect_args)
 
+# Cria uma fábrica de sessões (SessionLocal) para interagir com o banco
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 Base = declarative_base()
 
+# Função utilitária para injetar uma sessão no FastAPI
 def get_db():
     from fastapi import Depends
     db = SessionLocal()
